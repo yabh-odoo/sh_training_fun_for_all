65a585f7da5ae8521175629e57a86c4ada015475
Moens Alexandre <mao@odoo.com>
2023-05-12 11:24:42 +0200

    nuking this thing

--- pos_discount_with_a_twist/__manifest__.py
+++ pos_discount_with_a_twist/__manifest__.py
@@ -15,17 +15,9 @@ discount to a customer.
 
 """,
     'depends': ['point_of_sale'],
-    'data': [
-        'views/pos_discount_views.xml',
-        ],
+    # 'data': [
+    #     'views/pos_discount_views.xml',
+    #     ],
     'installable': True,
-    'assets': {
-        'point_of_sale.assets': [
-            'pos_discount/static/src/js/**/*',
-        ],
-        'web.assets_qweb': [
-            'pos_discount/static/src/xml/**/*',
-        ],
-    },
     'license': 'LGPL-3',
 }
--- pos_discount_with_a_twist/models/pos_config.py
+++ pos_discount_with_a_twist/models/pos_config.py
@@ -13,12 +13,12 @@ class PosConfig(models.Model):
         domain="[('sale_ok', '=', True)]", help='The product used to model the discount.')
 
     @api.onchange('company_id','module_pos_discount')
-    def _default_discount_product_id(self):
+    def _default_discount_product_idfhdjskqlgfdsqfldhsqjk(self):
         product = self.env.ref("point_of_sale.product_product_consumable", raise_if_not_found=False)
         self.discount_product_id = product if self.module_pos_discount and product and (not product.company_id or product.company_id == self.company_id) else False
 
     @api.model
-    def _default_discount_value_on_module_install(self):
+    def _default_discount_value_on_module_install_fhdjsqkgfkd(self):
         configs = self.env['pos.config'].search([])
         open_configs = (
             self.env['pos.session']
